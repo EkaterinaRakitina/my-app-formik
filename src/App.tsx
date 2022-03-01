76a9a3ffdf0374f36e44076f746ea4fb1d4e67f4
EkaterinaRakitina <erakitina.exceedteam@gmail.com>
2022-03-01 10:11:13 +0300

    add formik, create form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Formik, Field, Form } from "formik";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h1>My Form</h1>
+        <Formik
+          initialValues={{
+            lastName: "",
+            firstName: "",
+            fullName: "",
+            birthDate: "",
+            sex: "",
+          }}
+          // onSubmit={values => console.log(values)}
+          onSubmit={(values, actions) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              actions.setSubmitting(false);
+            }, 500);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {(props) => (
+            <Form>
+              <label htmlFor="lastName">Last name: </label>
+              <Field type="text" name="lastName" id="lastName" />
+              <br />
+              <label htmlFor="firstName">First name: </label>
+              <Field type="text" name="firstName" id="firstName" />
+              <br />
+              <label htmlFor="fullName">Full name: </label>
+              <Field type="text" name="fullName" id="fullName" />
+              <br />
+              <label htmlFor="birthDate">Birth date: </label>
+              <Field type="date" name="birthDate" id="birthDate" />
+              <br />
+              <label htmlFor="sex">Sex: </label>
+              <Field as="select" name="sex" id="sex">
+                <option value="mail">Male</option>
+                <option value="femail">Female</option>
+              </Field>
+              <button type="submit">Submit</button>
+            </Form>
+          )}
+        </Formik>
+      </div>
     </div>
   );
 }
